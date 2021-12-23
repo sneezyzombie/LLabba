be814d6c63931cd4a65e4ad493fb0c54629b9858
sneezyzombiegit config --global user.name sneezyzombie$ git config --global user.name sneezyzombie <reddan1470@gmail.com>
2021-12-23 17:53:58 +0300

    Добавьте файлы проекта.

---
+++ LLabba/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLabba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            double x, y;
+
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+
+            double.TryParse(a, out x);
+            double.TryParse(b, out y);
+
+            Console.WriteLine("summ: " + (x + y));
+            Console.WriteLine("difference: " + (x - y));
+            Console.WriteLine("result of multiplication: " + (x * y));
+            Console.WriteLine("result of division: " + (x / y));
+            
+        }
+    }
+}
---
+++ radius/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string var;
+            double r;
+
+            var = Console.ReadLine();
+            double.TryParse(var, out r);
+
+            Console.WriteLine("lenght: " + (2 * 3.14 * r));
+            Console.WriteLine("s: " + (3.14 * r * r));
+        }
+    }
+}
